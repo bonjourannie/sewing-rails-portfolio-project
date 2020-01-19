a73d2eb40ba57ebc72018bc2d8db64cda1d9bdf1
bonjourannie <artruex@gmail.com>
2020-01-18 19:23:29 -0800

    add first methods to project model

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,11 @@ class Category < ApplicationRecord
     has_many :projetcs, through: :project_materials
     validates :name, presence: :true 
     validates_uniqueness_of :name 
+
+    scope :sort_by_popularity, -> { joins(:projects)
+    .select("categories.*", "COUNT(projects.id) AS projects_count")
+    .group(:category_id)
+    .order("projects_count DESC")
+  }
+   scope :sort_ABC, -> { order("NAME ASC")}
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -7,4 +7,32 @@ class Project < ApplicationRecord
 
     validates_presence_of :name, :materials, :instructions
     validates_uniqueness_of :name
+
+    def materials_attributes=(projects_attributes)
+        material_name = material_name[:name]
+        material = Material.find_or_create_by(name: material_name)
+        if !material_name.empty? && material
+            self.materials << material unless self.materials.include?(material)
+        end 
+        self.save
+    end
+
+    def categories_attributes=(categories_attributes)
+        categories_attributes.values.each do |category_attributes|
+            if !categories_attribute[:name].empty? && category = Category.find_or_create_by(categories_attribute)
+                self.categories << category 
+            end
+        end
+    end
+
+    def self.most_materials 
+        left_joins(:materials)
+        .group(:id)
+        .order('COUNT(materials.id) DESC')
+        .limit(1)
+    end
+
+    def self.search(search)
+        joins(:materials).where({materials: {name: "#{search}"}})
+    end
 end

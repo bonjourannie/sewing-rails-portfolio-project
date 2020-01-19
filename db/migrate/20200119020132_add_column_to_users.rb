857be151600eec10507847b9f499747478ef553b
bonjourannie <artruex@gmail.com>
2020-01-19 09:01:53 -0800

    add projects_count to user table with default 0

--- db/migrate/20200119020132_add_column_to_users.rb
+++ db/migrate/20200119020132_add_column_to_users.rb
@@ -1,6 +1,6 @@
-class AddColumnToUserTable < ActiveRecord::Migration[6.0]
+class AddColumnToUsers < ActiveRecord::Migration[6.0]
   def change
-    add_column :users, :projects_count, :integer
+    add_column :users, :projects_count, :integer, :default => 0
     User.reset_column_information
     User.all.each do |u|
       User.update_counters u.id, :projects_count => u.projects.length

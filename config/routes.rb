7990d694ae97f806e2183db78f4fe28647c05cf4
bonjourannie <artruex@gmail.com>
2020-01-20 12:27:40 -0800

    routes for singup, login, logout and user controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  root to: 'users#welcome'
+
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to 'sessions#destroy', as: 'logout'
+
   resources :projects
   resources :project_materials
   resources :materials

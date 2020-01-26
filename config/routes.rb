551129f8a6adb0e4475b21f3738486ef3a297936
bonjourannie <artruex@gmail.com>
2020-01-26 14:07:32 -0800

    add routes back in

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   
   root 'users#welcome'
 
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+  
   get '/projects/most_materials', to: 'projects#most_materials', as: "projects_most_materials"
   get '/projects/:id/materials', to: 'project_materials#index', as: "project_material_index"
   get '/projects/:id/materials/new', to: 'project_materials#new', as: "project_material_new"

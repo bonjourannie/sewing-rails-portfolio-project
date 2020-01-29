a8adb1444ecc97ca09301c03e0b0e829c30669ee
bonjourannie <artruex@gmail.com>
2020-01-29 13:26:02 -0800

    update project_materials_controller

--- app/controllers/project_materials_controller.rb
+++ app/controllers/project_materials_controller.rb
@@ -1,2 +1,24 @@
 class ProjectMaterialsController < ApplicationController
+    def edit
+        if @project.user == current_user
+          5.times{@project.materials.build}
+        else
+          redirect_to root_path
+          flash[:notice] = "You can only edit your own projects."
+        end
+    end
+    
+    def update
+        @project.update(project_params)
+        if @project.save
+          render :show
+        else
+          render :edit
+        end
+    end
+    
+    def most_materials
+        project = Project.most_materials
+        @project = Project.find(proejct[0].id)
+    end
 end

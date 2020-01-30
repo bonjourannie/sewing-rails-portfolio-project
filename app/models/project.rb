107dd297b6f5aefe05943d8e12d025b85824535c
bonjourannie <artruex@gmail.com>
2020-01-30 12:25:01 -0800

    add methods to project model to find by name

--- app/models/project.rb
+++ app/models/project.rb
@@ -8,13 +8,15 @@ class Project < ApplicationRecord
     validates_presence_of :name, :materials, :instructions
     validates_uniqueness_of :name
 
+    scope :find_projetcs_by_name, -> (name) { where("name like ?", "#{name.capitalize}%")}
+
     def project_materials_attributes=(project_materials_hash)
 		project_materials_hash.values.each do |project_material|
 			project = Project.find_by(id: project_material[:project_id])
 			proj_material = ProjectMaterial.find_by(id: project_material[:id])
 			material = Material.find_by(id: project_material[:material_attributes][:id])
 			if project && proj_material && material
-				proj_material.update(notes: project_material[:notes])
+				proj_material.update(amount: project_material[:amount])
 				material.update(name: project_material[:material_attributes][:name])
 			else
 				self.project_materials.build(project_material)
@@ -30,6 +32,21 @@ class Project < ApplicationRecord
         end
     end
 
+    def ingredient_names 
+		self.recipe_ingredients.map(&:ingredient_id)
+		if ingredient_ids != ingredient_ids.uniq
+			self.errors.add(:recipe_ingredients, "Ingredient Name Should Appear Once!")
+		end
+	end
+
+	def self.list_by_category 
+		all.group_by(&:category)
+	end
+	
+	def self.desc_listing
+		all.order(created_at: :desc)
+	end
+
     def self.most_materials 
         left_joins(:materials)
         .group(:id)
@@ -40,4 +57,10 @@ class Project < ApplicationRecord
     def self.search(search)
         joins(:materials).where({materials: {name: "#{search}"}})
     end
+
+    private 
+
+	def name_capitalizer 
+		self.name = self.name.capitalize 
+	end
 end
